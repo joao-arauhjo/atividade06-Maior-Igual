1565198249603f0cab8e75a7d5c87dadd4a43df3
JohnAra <87151137+joao-arauhjo@users.noreply.github.com>
2021-08-19 19:45:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//Exercício 6 - Elabore um programa que recebe dois valores inteiros e mostra se o primeiro valor é maior ou igual ao segundo valor//
+console.log('O primeiro valor é MAIOR ou IGUAL ao segundo?')
+let valor1 = prompt("Digite um valor: ");
+let valor2 = prompt("Digite outro valor: ");
+let resultado = valor1 >= valor2
+let imprimir = console.log(resultado)
